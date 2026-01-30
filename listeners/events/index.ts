bfe9469f3707d86f9bc5fd8107acf2cef8be9f3c
Tim Tran <121896621+Drummingcoder@users.noreply.github.com>
2026-01-30 17:01:38 +0000

    No more undefined string

--- listeners/events/index.ts
+++ listeners/events/index.ts
@@ -256,7 +256,7 @@ const register = (app: App) => {
         ? new Date(event.subteam.date_create * 1000)
         : null;
       const datestring = thedate ? thedate.toLocaleString() : 'unknown';
-      const user = await client.users.info({ user: event.subteam.deleted_by });
+      const user = await client.users.info({ user: (event.subteam as any).deleted_by });
       messandstore(client, 'Subteam Deleted', `@${event.subteam.handle} was deleted by <@${event.subteam.deleted_by}>. Details:\n
         Date created: ${datestring}\n
         Name: ${event.subteam.name}\n
