5edcbae76765328e7d2c30d6531f2a65fe15c808
Tim Tran <121896621+Drummingcoder@users.noreply.github.com>
2026-02-17 23:27:00 +0000

    Method event doesn't exist

--- listeners/messages/index.ts
+++ listeners/messages/index.ts
@@ -8,12 +8,12 @@ const register = (app: App) => {
   // app.message(/^(hi|hello|hey).*/, sampleMessageCallback);
 
   app.message(async ({ message, client, logger }) => {
-    if (message.event.subtype === 'channel_convert_to_public') {
-      await messandstore(client, 'Channel Made Public', `Channel <#${message.event.channel}> (id: ${message.event.channel}) is made public by <@${message.event.user}>.`, privChannel, logger);
-      //publicMessage(client, 'Channel Made Public', `Channel <#${message.event.channel}> (id: ${message.event.channel}) is made public by <@${message.event.user}>.`, pubChannel, logger);
-    } else if (message.event.subtype === 'channel_convert_to_private') { 
-      await messandstore(client, 'Channel Made Private', `Channel <#${message.event.channel}> (id: ${message.event.channel}) is made private by <@${message.event.user}>.`, privChannel, logger);
-      //publicMessage(client, 'Channel Made Private', `Channel <#${message.event.channel}> (id: ${message.event.channel}) is now private by <@${message.event.user}>.`, pubChannel, logger);
+    if (message.subtype === 'channel_convert_to_public') {
+      await messandstore(client, 'Channel Made Public', `Channel <#${message.channel}> (id: ${message.channel}) is made public by <@${message.user}>.`, privChannel, logger);
+      //publicMessage(client, 'Channel Made Public', `Channel <#${message.channel}> (id: ${message.channel}) is made public by <@${message.user}>.`, pubChannel, logger);
+    } else if (message.subtype === 'channel_convert_to_private') { 
+      await messandstore(client, 'Channel Made Private', `Channel <#${message.channel}> (id: ${message.channel}) is made private by <@${message.user}>.`, privChannel, logger);
+      //publicMessage(client, 'Channel Made Private', `Channel <#${message.channel}> (id: ${message.channel}) is now private by <@${message.user}>.`, pubChannel, logger);
     }
   });
 };
